880d6be1266869e9144c04d580cbc8627e48463e
nguyenthanhdat307 <nguyenthanhdat30072003@gmail.com>
2024-11-09 08:01:18 +0700

    Update 13.py

--- 13.py
+++ 13.py
@@ -2,3 +2,4 @@ https://drive.google.com/file/d/1eDlnek0NWc8OrK4g_iffFkFPXWw2oaKE/view?usp=shari
 https://drive.google.com/file/d/1pEFF-mX6x3JiELp-pUeaPScxsShj3ago/view?usp=sharing
 https://drive.google.com/file/d/1YWxMp3YDWCaKWuumorKE5OncEoUCrqJu/view?usp=sharing
 https://drive.google.com/file/d/1Y-wL4pxB_T-qsHFsnUPGupjwHnXmFZrK/view?usp=sharing
+https://drive.google.com/file/d/14wbjqP8FxX51YK8Ou1anYaN4KK-aH_Zc/view?usp=sharing
